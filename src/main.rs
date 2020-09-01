c1369ba2f138008c775441dbd2512aebf7c2f00f
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-09-01 22:10:47 +0200

    Add cargo and test

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,25 @@ fn fizzbuzz(number: u32)-> String {
         (0, _) => "fizz".to_string(),
         (_, 0) => "buzz".to_string(),
         (_, _) => number.to_string()
-        }
+    }
 }
 
 
 fn count_up_to (number: u32) -> () {
     for i in 1..=number {
         println!("{}", fizzbuzz(i))
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::fizzbuzz;
+    #[test]
+    fn test_fizzbuzz() {
+        assert_eq!(fizzbuzz(3), "fizz");
+        assert_eq!(fizzbuzz(5), "buzz");
+        assert_eq!(fizzbuzz(15), "fizzbuzz");
+        assert_eq!(fizzbuzz(4), "4");
     }
 }

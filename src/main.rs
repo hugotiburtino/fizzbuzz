a20d5f4ed41f51fbc1743463e73628173ba94cc4
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-09-03 12:41:53 +0200

    Create parse input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use std::io;
+
 fn main() {
-    count_up_to(100);
+    wellcome();
 }
+
+
+fn wellcome() {
+    println!("Fizzbuzz, please enter a the number");
+
+    let mut number = String::new();
+
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
+
+    let number: u32 = parse_input(number);
+    
+    count_up_to(number);
+}
+
 
 fn fizzbuzz(number: u32)-> String {
     match(number % 3, number % 5) {
@@ -19,14 +37,30 @@ fn count_up_to (number: u32) -> () {
 }
 
 
+fn parse_input(input: String) -> u32 {
+    let number: u32 = match input.trim()
+        .parse() {
+            Ok(num) => num,
+            Err(_) => panic!("{} is not a positive number", input.trim()),
+        };
+    number
+}
+
+
 #[cfg(test)]
 mod tests {
-    use super::fizzbuzz;
+    use super::*;
     #[test]
     fn test_fizzbuzz() {
         assert_eq!(fizzbuzz(3), "fizz");
         assert_eq!(fizzbuzz(5), "buzz");
         assert_eq!(fizzbuzz(15), "fizzbuzz");
         assert_eq!(fizzbuzz(4), "4");
+    }
+    #[test]
+    #[should_panic(expected = "is not a positive number")]
+    fn test_parse_input() {
+        parse_input("-50".to_string());
+        parse_input("I dont want to put a number".to_string());
     }
 }

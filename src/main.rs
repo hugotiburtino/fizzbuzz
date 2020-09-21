b739b838d92b08d9f95ca982203568c83bdad7e7
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-09-22 00:33:28 +0200

    Move code to lib

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::io;
 
+use fizzbuzz::count_up_to;
+use fizzbuzz::parse_input;
+
 fn main() {
     wellcome();
 }
@@ -17,50 +20,4 @@ fn wellcome() {
     let number: u32 = parse_input(number);
     
     count_up_to(number);
-}
-
-
-fn fizzbuzz(number: u32)-> String {
-    match(number % 3, number % 5) {
-        (0, 0) => "fizzbuzz".to_string(),
-        (0, _) => "fizz".to_string(),
-        (_, 0) => "buzz".to_string(),
-        (_, _) => number.to_string()
-    }
-}
-
-
-fn count_up_to (number: u32) -> () {
-    for i in 1..=number {
-        println!("{}", fizzbuzz(i))
-    }
-}
-
-
-fn parse_input(input: String) -> u32 {
-    let number: u32 = match input.trim()
-        .parse() {
-            Ok(num) => num,
-            Err(_) => panic!("{} is not a positive number", input.trim()),
-        };
-    number
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_fizzbuzz() {
-        assert_eq!(fizzbuzz(3), "fizz");
-        assert_eq!(fizzbuzz(5), "buzz");
-        assert_eq!(fizzbuzz(15), "fizzbuzz");
-        assert_eq!(fizzbuzz(4), "4");
-    }
-    #[test]
-    #[should_panic(expected = "is not a positive number")]
-    fn test_parse_input() {
-        parse_input("-50".to_string());
-        parse_input("I dont want to put a number".to_string());
-    }
 }
